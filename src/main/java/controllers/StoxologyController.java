48f870db9418646ae0af4e5815668e4adf8b2c18
Jaime Clinton <jaime.clinton3@gmail.com>
2015-12-06 11:19:43 +0000

    Initial commit.

---
+++ src/main/java/controllers/StoxologyController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoxologyController {
+
+    @RequestMapping("/")
+	public String index() {	
+		return "Greetings from Spring Boot!";
+	}
+}
