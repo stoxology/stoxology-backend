bd93ce2f5b1abe91504c1a52c60feabec7291a44
James Butler <jamesbutler28@googlemail.com>
2016-02-06 17:31:16 +0000

    Minor update to handle errors, included a data grab for 1 year from Feb

--- src/main/java/stoxology/Main.java
+++ src/main/java/stoxology/Main.java
@@ -14,6 +14,7 @@ import stoxology.datacollator.DataRetrieval;
 import stoxology.datacollator.twitterapi.TwitterDataRetrievalImpl;
 import stoxology.datacollator.twitterapi.entities.TwitterDataExtract;
 import stoxology.datacollator.alchemy.AlchemyExtractor;
+import stoxology.datacollator.alchemy.entities.KeywordResult;
 
 @SpringBootApplication
 @Configuration
@@ -31,7 +32,12 @@ public class Main {
 		{
 			for (String url : tde.getUrlsOfInterest())
 			{
-				tde.getKeywordResults().add(alcExtract.extractAll(url));
+				KeywordResult keywordResult = alcExtract.extractAll(url);
+				
+				if (keywordResult == null)				
+					continue;				
+				
+				tde.getKeywordResults().add(keywordResult);
 			}
 		}
 		
